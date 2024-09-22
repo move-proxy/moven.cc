45471e5623507728f6f3c0d71922746759e3274f
move-proxy <suu3028@gmail.com>
2024-09-22 19:57:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-// Smooth scroll to section
-function scrollToSection(id) {
-    document.querySelector(id).scrollIntoView({
-        behavior: 'smooth'
-    });
-}
+const modal = document.getElementById("loginModal");
+const span = document.getElementsByClassName("close")[0];
+const mainContent = document.getElementById("mainContent"); 
 
-// Form submission (dummy for now)
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Your message has been sent!');
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
+
+
+const validUsername = atob('bW92ZQ=='); 
+const validPassword = atob('aWxvdmV3ZWVkQDEyMw=='); 
+
+
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault(); 
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+   
+
+  
+    if (username === validUsername && password === validPassword) {
+        alert('Login successful!');
+        modal.style.display = 'none'; 
+        mainContent.style.display = 'block'; 
+    } else {
+        alert('Invalid username or password!');
+    }
 });
+
+window.onload = function() {
+    modal.style.display = "block"; 
+};
