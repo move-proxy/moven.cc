ba08da6aeadc7047bc67871c7b295c0c9d1cadb7
move-proxy <suu3028@gmail.com>
2024-09-23 20:16:46 +0200

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,21 +1,29 @@
 exports.handler = async (event, context) => {
-  // Get username and password from the environment variables
-  const validUsername = process.env.VALID_USERNAME;
-  const validPassword = process.env.VALID_PASSWORD;
+    // Check if the request is a POST
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ message: 'Method Not Allowed' }),
+        };
+    }
 
-  // Parse the body of the request (sent from the frontend)
-  const { username, password } = JSON.parse(event.body);
+    // Get username and password from environment variables
+    const validUsername = process.env.VALID_USERNAME;
+    const validPassword = process.env.VALID_PASSWORD;
 
-  // Check credentials
-  if (username === validUsername && password === validPassword) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Login successful!' }),
-    };
-  } else {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ message: 'Invalid username or password!' }),
-    };
-  }
+    // Parse the body of the request
+    const { username, password } = JSON.parse(event.body);
+
+    // Check credentials
+    if (username === validUsername && password === validPassword) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Login successful!' }),
+        };
+    } else {
+        return {
+            statusCode: 401,
+            body: JSON.stringify({ message: 'Invalid username or password!' }),
+        };
+    }
 };
